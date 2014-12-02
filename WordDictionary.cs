1355dc91f6455ec67b936943abca85efa4920cb1
Brian Hamachek <brian@brianhama.com>
2014-12-02 12:08:53 -0800

    Using multiple buckets based upon string prefix.  Very large performance improvement.

--- WordDictionary.cs
+++ WordDictionary.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 
 namespace BoggleSolver
 {
-    public sealed class WordDictionary : SortedSet<String>
+    public sealed class WordDictionary
     {
+        private readonly Dictionary<String, SortedSet<String>> _words = new Dictionary<string, SortedSet<string>>();
+
         private WordDictionary()
         {
         }
@@ -29,7 +32,9 @@ namespace BoggleSolver
             {
                 while (!reader.EndOfStream)
                 {
-                    dictionary.Add(reader.ReadLine().ToLower());
+                    string word = reader.ReadLine();
+                    if (word.Length > 2)
+                        dictionary.AddWordToDictionary(word.ToLower());
                 }
                 reader.Close();
             }
@@ -37,15 +42,38 @@ namespace BoggleSolver
             return dictionary;
         }
 
+        private void AddWordToDictionary(string word)
+        {
+            string prefix = word.Substring(0, 2);
+            if (_words.ContainsKey(prefix))
+                _words[prefix].Add(word);
+            else
+            {
+                SortedSet<String> newBucket = new SortedSet<string>();
+                newBucket.Add(word);
+                _words.Add(prefix, newBucket);
+            }
+        }
+
         public LookupResult LookupWord(string word)
         {
-            if (Contains(word))
-                return LookupResult.Match;
-
-            if (this.Where(a => a.Length >= word.Length).Select(b => b.Substring(0, word.Length)).Contains(word))
+            if (word.Length <= 2)
                 return LookupResult.PartialMatch;
 
-            return LookupResult.NoMatch;
+            string prefix = word.Substring(0, 2);
+
+            if (!_words.ContainsKey(prefix))
+                return LookupResult.NoMatch;
+
+            SortedSet<String> bucket = _words[prefix];
+
+            if (!bucket.Any(a => a.StartsWith(word)))
+                return LookupResult.NoMatch;
+
+            if (bucket.Contains(word))
+                return LookupResult.Match;
+
+            return LookupResult.PartialMatch;
         }
     }
 }
